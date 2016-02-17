f891a6fd03cc91f489f176f6d1ad3e1bd046512c
klarahirao <klara.hirao@elpassion.pl>
2016-02-17 10:47:43 +0100

    Add price to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217083557) do
+ActiveRecord::Schema.define(version: 20160217094355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,9 @@ ActiveRecord::Schema.define(version: 20160217083557) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.decimal  "price",      precision: 8, scale: 2
   end
 
   create_table "users", force: :cascade do |t|
