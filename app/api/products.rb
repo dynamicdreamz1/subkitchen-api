719b91144fa97ca86d8bc6191538b7f5c8ca27bb
klarahirao <klara.hirao@elpassion.pl>
2016-02-22 13:13:24 +0100

    Add create/remove product api

--- app/api/products.rb
+++ app/api/products.rb
@@ -8,18 +8,46 @@ module Products
       %w(http://lorempixel.com/300/300/)
     end
     resources :products do
-      desc 'Return all products'
+      desc 'return all products'
       get '' do
         page = params[:page].to_i != 0? params[:page] : 1
         per_page = params[:per_page].to_i != 0? params[:per_page] : 30
         Product.page(page).per(per_page)
       end
 
-      desc 'Return product by id'
+      desc 'return product by id'
       get ':id' do
         product = Product.find(params[:id])
         if product
           ProductSerializer.serialize(product)
+        else
+          status :unprocessable_entity
+        end
+      end
+
+      desc 'create product'
+      params do
+        requires :name, type: String
+      end
+      post 'create' do
+        authenticate!
+        product = Product.create(name: params.name, user_id: current_user.id)
+        if product
+          product
+        else
+          status :unprocessable_entity
+        end
+      end
+
+      desc 'remove product'
+      params do
+        requires :product_id, type: Integer
+      end
+      delete 'remove' do
+        authenticate!
+        product = Product.find_by(id: params.product_id, user_id: current_user.id)
+        if product
+          product.destroy
         else
           status :unprocessable_entity
         end
