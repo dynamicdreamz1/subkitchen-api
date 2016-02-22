7003aeb04976aa25719cb6c4ef685798ef4226dd
klarahirao <klara.hirao@elpassion.pl>
2016-02-22 13:10:23 +0100

    Add notification routes and controller

---
+++ app/controllers/user_verify_notifications_controller.rb
@@ -0,0 +1,12 @@
+class UserVerifyNotificationsController < ApplicationController
+  protect_from_forgery except: [:create]
+
+  def create
+    params.permit!
+    UserVerifyNotification.create!(params: params,
+                                order_id: params[:invoice],
+                                status: params[:payment_status],
+                                transaction_id: params[:txn_id])
+    render :nothing => true
+  end
+end
