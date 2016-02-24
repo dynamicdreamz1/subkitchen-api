65fd2cb3fbfbb4a6180775a8d80afeeedce27edd
Rafał Lisowski <lisukorin@gmail.com>
2016-02-24 10:37:55 +0100

    use s3 only on production

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,12 @@
-require 'refile/s3'
+if Rails.env.production?
+    require 'refile/s3'
 
-aws = {
-    access_key_id: Figaro.env.s3_access_key,
-    secret_access_key: Figaro.env.s3_secret,
-    region: "eu-west-1",
-    bucket: Figaro.env.s3_bucket,
-}
-Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-Refile.store = Refile::S3.new(prefix: "store", **aws)
+    aws = {
+        access_key_id: Figaro.env.s3_access_key,
+        secret_access_key: Figaro.env.s3_secret,
+        region: "eu-west-1",
+        bucket: Figaro.env.s3_bucket,
+    }
+    Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+    Refile.store = Refile::S3.new(prefix: "store", **aws)
+end
