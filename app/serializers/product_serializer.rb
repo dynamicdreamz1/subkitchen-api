ddb0018fab9e9f8d485ba83813810bd08c6502c5
klarahirao <klara.hirao@elpassion.pl>
2016-02-29 11:43:04 +0100

    Add product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,18 +1,22 @@
 class ProductSerializer
   def as_json(options={})
-    data = { id: product.id,
-      author: product.author.name,
-      price: product.product_template.price.to_s,
-      size: product.product_template.size,
-      description: product.description,
-      name: product.name,
-      likes: product.likes,
-      product_image: product.image_url,
-      shipping: Config.shipping_info,
-      shipping_cost: Config.shipping_cost,
-      tax: Config.tax,
-      size_chart: product.product_template.size_chart_url }
-
+    data = {
+      product: {
+          id: product.id,
+          is_deleted: product.is_deleted,
+          author: (product.author ? product.author.name : nil),
+          price: product.product_template.price.to_s,
+          size: product.product_template.size,
+          description: product.description,
+          name: product.name,
+          likes: product.likes,
+          product_image: product.image_url,
+          shipping: Config.shipping_info,
+          shipping_cost: Config.shipping_cost,
+          tax: Config.tax,
+          size_chart: product.product_template.size_chart_url
+      }
+    }
     data[:errors] = product.errors if product.errors.any?
 
     data.as_json(options)
@@ -20,7 +24,7 @@ class ProductSerializer
 
   private
 
-  attr_reader :product
+  attr_accessor :product
 
   def initialize(product)
     @product = product
