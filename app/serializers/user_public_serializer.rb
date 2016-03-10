30cff3056959d46ccf3850e5f2668fd232722e0f
Rafał Lisowski <lisukorin@gmail.com>
2016-03-10 17:43:24 +0100

    #SK-73 user avatar upload

--- app/serializers/user_public_serializer.rb
+++ app/serializers/user_public_serializer.rb
@@ -1,7 +1,7 @@
 class UserPublicSerializer
   def as_json(options = {})
     data = { name: user.name,
-      image_url: (user.profile_image_url ? user.profile_image_url : image_url),
+      image_url: image_url,
       artist: user.artist,
       handle: user.handle,
       status: user.status,
@@ -25,9 +25,13 @@ class UserPublicSerializer
   end
 
   def image_url
-    if user.provider == 'facebook' && user.uid
-      return "https://graph.facebook.com/#{user.uid}/picture?width=200"
+    if user.profile_image_url
+      Figaro.env.app_host + Refile.attachment_url(user, :profile_image, format: :png)
+    else
+      if user.provider == 'facebook' && user.uid
+        return "https://graph.facebook.com/#{user.uid}/picture?width=200"
+      end
+      'assets/images/default.png'
     end
-    nil
   end
 end
