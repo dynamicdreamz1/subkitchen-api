4722e9677ec27491798e460cbd0b7af1db242a18
Rafał Lisowski <lisukorin@gmail.com>
2016-03-11 16:13:02 +0100

    #SK-55 #SK-71 user can like/unlike product

--- spec/requests/products/product_like_spec.rb
+++ spec/requests/products/product_like_spec.rb
@@ -9,44 +9,33 @@ describe Products::Api, type: :request do
   describe '/api/v1/products' do
     describe 'LIKE product' do
       it 'should like product' do
-        post "/api/v1/products/#{product.id}/likes", {}, auth_header_for(artist)
+        post "/api/v1/products/#{product.id}/toggle_like", {}, auth_header_for(artist)
 
+        expect(response).to have_http_status(:success)
         product.reload
         expect(json['likes']).to eq(1)
       end
 
       it 'should not like own product' do
-        post "/api/v1/products/#{artist_product.id}/likes", {}, auth_header_for(artist)
+        post "/api/v1/products/#{artist_product.id}/toggle_like", {}, auth_header_for(artist)
 
+        expect(response).to have_http_status(:unprocessable_entity)
         product.reload
         expect(product.likes.count).to eq(0)
         expect(json['errors']).to eq({'base'=>['cannot like own product']})
-      end
-
-      it 'should not like product twice' do
-        create(:like, likeable_id: product.id, likeable_type: product.class.name, user: artist)
-
-        post "/api/v1/products/#{product.id}/likes", {}, auth_header_for(artist)
-
-        product.reload
-        expect(product.likes.count).to eq(1)
-        expect(json['errors']).to eq({'base'=>['cannot like product more than once']})
       end
     end
 
     describe 'UNLIKE product' do
       it 'should unlike product' do
         create(:like, likeable_id: product.id, likeable_type: product.class.name, user: artist)
+        expect(product.likes.count).to eq(1)
 
-        delete "/api/v1/products/#{product.id}/likes", {}, auth_header_for(artist)
+        post "/api/v1/products/#{product.id}/toggle_like", {}, auth_header_for(artist)
 
+        expect(response).to have_http_status(:success)
         expect(json['likes']).to eq(0)
-      end
-
-      it 'should not unlike product when product not liked' do
-        delete "/api/v1/products/#{product.id}/likes", {}, auth_header_for(artist)
-
-        expect(json['errors']).to eq({'base'=>['no like with given user id']})
+        expect(product.likes.count).to eq(0)
       end
     end
   end
