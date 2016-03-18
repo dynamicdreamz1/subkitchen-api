53db1ce91b6ec9b3f11436e4341bc95bd57dd8e0
Rafał Lisowski <lisukorin@gmail.com>
2016-03-18 14:32:24 +0100

    SK-78 check-out

--- app/services/paypal_payment.rb
+++ app/services/paypal_payment.rb
@@ -20,12 +20,14 @@ class PaypalPayment
         notify_url: Figaro.env.app_host+"/api/v1/payment_notification"
     }
     @payment.payable.order_items.each_with_index do |item, index|
-      values.merge!({
-                        "amount_#{index+1}" => item.price,
-                        "item_name_#{index+1}" => item.product.name,
-                        "item_number_#{index+1}" => item.id,
-                        "quantity_#{index+1}" => item.quantity
-                    })
+      if item.quantity > 0
+        values.merge!({
+          "amount_#{index+1}" => item.price,
+          "item_name_#{index+1}" => item.product.name,
+          "item_number_#{index+1}" => item.id,
+          "quantity_#{index+1}" => item.quantity
+        })
+      end
     end
     "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
   end
