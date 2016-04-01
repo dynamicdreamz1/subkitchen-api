008754c4a27fa055394f63ea4000af6e5721894d
Rafał Lisowski <lisukorin@gmail.com>
2016-04-01 13:39:12 +0200

    mailers refactor

--- spec/mailers/previews/account_reset_password_preview.rb
+++ spec/mailers/previews/account_reset_password_preview.rb
@@ -1,6 +1,8 @@
 class AccountResetPasswordPreview < ActionMailer::Preview
   def reset_password
     user = User.first
-    AccountResetPassword.notify(user)
+    options = { password_reminder_token: user.password_reminder_token,
+                name: user.name }
+    AccountResetPassword.notify(user.email, options)
   end
 end
