1095c3b31a4e4363e596c986fe0510cd8f8c7552
klarahirao <klara.hirao@elpassion.pl>
2016-04-07 18:21:51 +0200

    Add order confirmation mailer preview

---
+++ spec/mailers/previews/order_confirmation_mailer_preview.rb
@@ -0,0 +1,8 @@
+class OrderConfirmationMailerPreview < ActionMailer::Preview
+  def send_invoice
+    order = Order.create(purchased_at: DateTime.now)
+    invoice = Invoice.create(order: order, line_2: 'SubKitchen')
+    options = {order: order, invoice: invoice}
+    OrderConfirmationMailer.notify('test@example.com', options)
+  end
+end
