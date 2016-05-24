e962da87aae617ecfa7ce464319936a8d5a5933d
klarahirao <klara.hirao@elpassion.pl>
2016-05-24 15:00:06 +0200

    Cc refactor

--- app/serializers/order_serializers/checkout_serializer.rb
+++ app/serializers/order_serializers/checkout_serializer.rb
@@ -2,58 +2,81 @@ class CheckoutSerializer
   include ApplicationHelper
 
   def as_json(options = {})
-    data = { order:
-             { id: order.id,
-               uuid: order.uuid,
-               purchased_at: order.purchased_at,
-               status: order.order_status,
-               subtotal: order.subtotal_cost,
-               shipping_cost: order.shipping_cost,
-               tax: order.tax,
-               tax_cost: order.tax_cost,
-               total_cost: order.total_cost,
-               discount: order.discount,
-               items: items,
-               invoice_id: "#{order.id}/#{order.created_at.strftime('%d/%m/%Y')}" } }
-
-    if order.invoice
-      data[:order][:pdf] = Figaro.env.app_host + "/api/v1/invoices?uuid=#{order.uuid}"
-      data[:order][:placed] = "#{order.invoice.created_at.strftime('%B %d, %Y - %I:%M %p %Z')}"
-    end
-    data[:order][:shipping_address] = shipping_address if order.address
-    data[:errors] = order.errors if order.errors.any?
-    data[:deleted_items] = deleted_items if @deleted_items
+    add_invoice_data if order.invoice
+    add_shipping_address if order.address
+    add_errors if order.errors.any?
+    add_deleted_items if deleted_items
 
     data.as_json(options)
   end
 
   private
 
-  attr_accessor :order
+  attr_accessor :order, :data
+  attr_reader :deleted_items
 
-  def initialize(order, deleted_items = nil)
+  def initialize(order, deleted_items=nil)
     @order = order
+    @data = { order: order_hash }
     @deleted_items = deleted_items
   end
 
+  def add_invoice_data
+    data[:order][:pdf] = Figaro.env.app_host + "/api/v1/invoices?uuid=#{order.uuid}"
+    data[:order][:placed] = order.invoice.created_at.strftime('%B %d, %Y - %I:%M %p %Z').to_s
+  end
+
+  def add_shipping_address
+    data[:order][:shipping_address] = shipping_address
+  end
+
+  def add_errors
+    data[:errors] = order.errors
+  end
+
+  def add_deleted_items
+    data[:deleted_items] = deleted_items_hash
+  end
+
+  def order_hash
+    { id: order.id,
+      uuid: order.uuid,
+      purchased_at: order.purchased_at,
+      status: order.order_status,
+      subtotal: order.subtotal_cost,
+      shipping_cost: order.shipping_cost,
+      tax: order.tax,
+      tax_cost: order.tax_cost,
+      total_cost: order.total_cost,
+      discount: order.discount,
+      items: items_hash,
+      invoice_id: "#{order.id}/#{order.created_at.strftime('%d/%m/%Y')}" }
+  end
+
   def items
-    order.order_items
-         .reject { |item| item.product.is_deleted }
-         .map do |item|
-      { price: item.price,
-        subtotal: item.price * item.quantity,
-        name: item.product.name,
-        id: item.id,
-        is_deleted: item.product.is_deleted,
-        quantity: item.quantity,
-        size: item.size,
-        preview_url: Figaro.env.app_host + Refile.attachment_url(item.product, :preview, format: :png),
-        product_id: item.product_id}
+    order.order_items.reject { |item| item.product.is_deleted }
+  end
+
+  def items_hash
+    items.map do |item|
+      item_hash(item)
     end
   end
 
-  def deleted_items
-    @deleted_items.map do |item|
+  def item_hash(item)
+    { price: item.price,
+      subtotal: item.price * item.quantity,
+      name: item.product.name,
+      id: item.id,
+      is_deleted: item.product.is_deleted,
+      quantity: item.quantity,
+      size: item.size,
+      preview_url: Figaro.env.app_host + Refile.attachment_url(item.product, :preview, format: :png),
+      product_id: item.product_id }
+  end
+
+  def deleted_items_hash
+    deleted_items.map do |item|
       { price: item.price,
         name: item.product.name,
         id: item.id,
