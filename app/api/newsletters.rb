3e5cd238a2a8cc4e629d11575915c6b1c83352f4
klarahirao <klara.hirao@elpassion.pl>
2016-06-13 02:02:16 +0200

    Fix newsletter receiver endpoint

--- app/api/newsletters.rb
+++ app/api/newsletters.rb
@@ -3,18 +3,22 @@ module Newsletters
     resource :newsletter_receivers do
       desc 'add email to newsletter'
       params do
-        requires :email, type: String
+        requires :newsletter_receiver, type: Hash do
+          requires :email, type: String
+        end
       end
       post do
-        NewsletterReceiver.create!(email: params.email)
+        NewsletterReceiver.create!(email: params.newsletter_receiver.email)
       end
 
       desc 'remove email from newsletter'
       params do
-        requires :email, type: String
+        requires :newsletter_receiver, type: Hash do
+          requires :email, type: String
+        end
       end
       delete do
-        receiver = NewsletterReceiver.find_by!(email: params.email)
+        receiver = NewsletterReceiver.find_by!(email: params.newsletter_receiver.email)
         receiver.delete
       end
     end
--- spec/requests/newsletter_receiver_spec.rb
+++ spec/requests/newsletter_receiver_spec.rb
@@ -1,6 +1,6 @@
 describe Newsletters::Api, type: :request do
   it 'should create new newsletter receiver' do
-    post '/api/v1/newsletter_receivers', email: 'test@example.com'
+    post '/api/v1/newsletter_receivers', { newsletter_receiver: { email: 'test@example.com' } }
 
     expect(response).to have_http_status(:success)
     expect(NewsletterReceiver.count).to eq(1)
@@ -8,7 +8,7 @@ describe Newsletters::Api, type: :request do
 
   it 'should return error when newsletter receiver exists' do
     receiver = create(:newsletter_receiver)
-    post '/api/v1/newsletter_receivers', email: receiver.email
+    post '/api/v1/newsletter_receivers', { newsletter_receiver: { email: receiver.email } }
 
     expect(response).to have_http_status(:unprocessable_entity)
     expect(json['errors']).to eq('base' => ['Validation failed: Email has already been taken'])
@@ -17,14 +17,14 @@ describe Newsletters::Api, type: :request do
   it 'should delete newsletter receiver' do
     create(:newsletter_receiver)
     receiver = create(:newsletter_receiver)
-    delete '/api/v1/newsletter_receivers', email: receiver.email
+    delete '/api/v1/newsletter_receivers', { newsletter_receiver: { email: receiver.email } }
 
     expect(response).to have_http_status(:success)
     expect(NewsletterReceiver.count).to eq(1)
   end
 
   it 'should return error when newsletter receiver does not exist' do
-    delete '/api/v1/newsletter_receivers', email: 'test@example.com'
+    delete '/api/v1/newsletter_receivers', { newsletter_receiver: { email: 'test@example.com' } }
 
     expect(response).to have_http_status(:not_found)
     expect(json['errors']).to eq('base' => ['record not found'])
