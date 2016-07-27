959d35988acf01daaf89e4ea002d2ec99857db53
Rafał Lisowski <lisukorin@gmail.com>
2016-07-27 17:03:32 +0200

    send verification notice to artist
    fixes SK-323

--- app/mailers/artist_confirmation.rb
+++ app/mailers/artist_confirmation.rb
@@ -1,12 +1,23 @@
-class ArtistConfirmation < ApplicationMailer
-  KEYS = {}
+require 'concerns/email_key_replacer'
 
-  def notify(email)
+class ArtistConfirmation < ApplicationMailer
+  KEYS = { 'LOGO_IMG' => " - logo png file" }.freeze
+  include EmailKeyReplacer
+
+  def notify(email, options={})
     template = EmailTemplate.where(name: self.class.name.to_s).first
     content = template.content
+
+    replace_keys(content, values(options))
 
     mail(to: email, subject: template.subject ) do |format|
       format.html { render html: content.html_safe }
     end
+  end
+
+  private
+
+  def values(options)
+    { 'logo_img'     => attachments['logo.png'].url }
   end
 end
