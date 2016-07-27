0ddb56c706679c00000e0cb78bfc56bc89d06d09
Rafał Lisowski <lisukorin@gmail.com>
2016-07-27 12:55:35 +0200

    send payment confirmation

--- app/services/payment_services/common_payment.rb
+++ app/services/payment_services/common_payment.rb
@@ -4,6 +4,7 @@ class CommonPayment
     update_payment
     send_order_and_create_invoice
     SalesAndEarningsCounter.perform_async(order.id)
+    PaymentConfirmationMailer.notify(order.email, invoice: order.invoice, order: order)
     payment
   end
 
@@ -16,27 +17,27 @@ class CommonPayment
     @payment = payment
     @order = @payment.payable
     @token = payment_token
-	end
+  end
 
-	def send_order_and_create_invoice
-		FindOrCreateInvoice.new(order).call
-		status = if CheckOrderIfReady.new(order).call
-			SendOrder.new(order).call
-			'cooking'
-		else
-			NotifyDesigners.new(order).call
-			'processing'
-		end
-		order.update(order_status: status)
-	end
+  def send_order_and_create_invoice
+    FindOrCreateInvoice.new(order).call
+    status = if CheckOrderIfReady.new(order).call
+               SendOrder.new(order).call
+               'cooking'
+             else
+               NotifyDesigners.new(order).call
+               'processing'
+             end
+    order.update(order_status: status)
+  end
 
-	def update_order
-		order.update(purchased_at: DateTime.now,
-								 purchased: true,
-								 active: false)
-	end
+  def update_order
+    order.update(purchased_at: DateTime.now,
+                 purchased: true,
+                 active: false)
+  end
 
-	def update_payment
-		payment.update(payment_token: token, payment_status: :completed)
-	end
+  def update_payment
+    payment.update(payment_token: token, payment_status: :completed)
+  end
 end
