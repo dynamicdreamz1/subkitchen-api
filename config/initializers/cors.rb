20cd9a96f504a519c98614d6806da13a746993ce
Rafał Lisowski <lisukorin@gmail.com>
2016-07-28 17:17:40 +0200

    allows public domain to connect

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, 'Rack::Cors', debug: Rails.env.development?, logger: (-> { Rails.logger }) do
   allow do
-    origins 'localhost', 'localhost:4200', '127.0.0.1:4200', '0.0.0.0:4200', '192.168.1.119:4200', 'subkitchen.herokuapp.com', 'subkitchen.surge.sh'
+    origins 'localhost', 'localhost:4200', '127.0.0.1:4200', '0.0.0.0:4200', '192.168.1.119:4200', 'subkitchen.herokuapp.com', 'subkitchen.surge.sh', 'www.sublimation.kitchen', 'sublimation.kitchen'
     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :patch, :head, :options]
   end
 end
