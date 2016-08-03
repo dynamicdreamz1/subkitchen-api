2a5d60d5031e045e6ae86c08536c5182d2be9ad7
Rafał Lisowski <lisukorin@gmail.com>
2016-08-03 10:24:56 +0200

    Change square banners res
    fixes: SK-346

--- app/models/config.rb
+++ app/models/config.rb
@@ -19,15 +19,15 @@ class Config < ActiveRecord::Base
     end
 
     def promo_1_img
-      where(name: 'promo_1_img').first.config_image_url(:fill, 400, 400, format: :png)
+      where(name: 'promo_1_img').first.config_image_url(:fill, 800, 800, format: :png)
     end
 
     def promo_2_img
-      where(name: 'promo_2_img').first.config_image_url(:fill, 400, 400, format: :png)
+      where(name: 'promo_2_img').first.config_image_url(:fill, 800, 800, format: :png)
     end
 
     def promo_3_img
-      where(name: 'promo_3_img').first.config_image_url(:fill, 400, 400, format: :png)
+      where(name: 'promo_3_img').first.config_image_url(:fill, 800, 800, format: :png)
     end
 
     def banner_url
