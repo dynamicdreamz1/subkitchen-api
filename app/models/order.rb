e6b37116385222d26c918686a878cea45be1add3
Rafał Lisowski <lisukorin@gmail.com>
2016-08-03 10:16:29 +0200

    remove "creating" from orders section of admin
    fixes: SK-351

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,12 +13,13 @@ class Order < ActiveRecord::Base
 
   validates_with AddressValidator, on: :update
 
+  scope :list_all, -> { where('order_status != 0') }
   scope :waiting_products, ->(order) { order.products.where(design_id: nil) }
   scope :processing, -> { where(order_status: 2) }
   scope :creating, -> { where(order_status: 0) }
   scope :payment_pending, -> { where(order_status: 1) }
-	scope :completed, -> { where(order_status: 4) }
-	scope :failed, -> { where(order_status: 5) }
+  scope :completed, -> { where(order_status: 4) }
+  scope :failed, -> { where(order_status: 5) }
   scope :user, -> (user_id) { where(user_id: user_id) }
 
   validates :full_name, presence: true, on: :address
@@ -29,7 +30,7 @@ class Order < ActiveRecord::Base
   validates :country,   presence: true, on: :address
   validates :email,     presence: true, email: true, on: :address
 
-	def record_number
-		"#{id}/#{created_at.strftime('%d/%m/%Y')}"
-	end
+  def record_number
+    "#{id}/#{created_at.strftime('%d/%m/%Y')}"
+  end
 end
