ded015c7ac05210888c6b11f04908f93a2be77bf
Rafał Lisowski <lisukorin@gmail.com>
2016-08-06 11:36:42 +0200

    save resources

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
 uri = URI.parse(ENV['REDISTOGO_URL'] || 'redis://localhost:6379')
-$redis = ConnectionPool::Wrapper.new(size: 5, timeout: 3) { Redis.new(url: uri) }
+limit = URI.parse(ENV['REDISTOGO_LIMIT'] || 2
+$redis = ConnectionPool::Wrapper.new(size: limit, timeout: 3) { Redis.new(url: uri) }
