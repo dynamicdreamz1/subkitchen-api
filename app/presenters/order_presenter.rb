1966f4b0b93c1e72445ff7d0b721515e0889a951
Rafał Lisowski <lisukorin@gmail.com>
2016-08-11 17:38:16 +0200

    csv

--- app/presenters/order_presenter.rb
+++ app/presenters/order_presenter.rb
@@ -21,22 +21,22 @@ class OrderPresenter
 		end
 
 		def headers
-			'Order;Brand;Design;Product Type;Binding Color;Size;Qty;Mockup Front;Mockup Back;'\
-			 'Image Front;Image Back;Order #;Order Paid;Item SKU;Item Name;Item Quantity;Item Unit Price;Item Size;'\
-			  'Order Date;Order Total;Tax Paid;Shipping Paid;Buyer Full Name;Buyer Email;Buyer Username;Address;City;State;'\
-			   'Postal Code;Country Code'
+			'Order,Brand,Design,Product Type,Binding Color,Size,Qty,Mockup Front,Mockup Back,'\
+			 'Image Front,Image Back,Order #,Order Paid,Item SKU,Item Name,Item Quantity,Item Unit Price,Item Size,'\
+			  'Order Date,Order Total,Tax Paid,Shipping Paid,Buyer Full Name,Buyer Email,Buyer Username,Address,City,State,'\
+			   'Postal Code,Country Code'
 		end
 
 		def item_to_csv(item)
 			order = item.order
-			"#{order.id};Sublimation Kitchen;Custom;#{item.product.product_template.product_type};"\
-			 "#{item.template_variant.color.name};#{item.size};#{item.quantity};#{product_preview(item)};;"\
-			 	 "#{item.product.uploaded_image};;#{order.id};#{order.purchased_at.try(:strftime,'%d/%m/%Y')};#{item.product.id};"\
-			 	  "#{item.product.name};#{item.quantity};#{item.price};#{item.size};"\
-			 	   "#{order.created_at.strftime('%d/%m/%Y')};#{order.total_cost};#{order.tax_cost};#{order.shipping_cost};"\
-			 	    "#{order.full_name};#{order.email};"\
-			 	     "#{order.user ? order.user.name : ''};#{order.address};"\
-							"#{order.city};#{order.region};#{order.zip};"\
+			"#{order.id},Sublimation Kitchen,Custom,#{item.product.product_template.product_type},,"\
+			 "#{item.size},#{item.quantity},#{product_preview(item)},,"\
+			 	 "#{item.product.uploaded_image},,#{order.id},#{order.purchased_at.try(:strftime,'%d/%m/%Y')},#{item.product.id},"\
+			 	  "#{item.product.name},#{item.quantity},#{item.price},#{item.size},"\
+			 	   "#{order.created_at.strftime('%d/%m/%Y')},#{order.total_cost},#{order.tax_cost},#{order.shipping_cost},"\
+			 	    "#{order.full_name},#{order.email},"\
+			 	     "#{order.user ? order.user.name : ''},#{order.address},"\
+							"#{order.city},#{order.region},#{order.zip},"\
 							 "#{order.country}"
 		end
 
