dcc20d5e17859bc379f47260f6e1fadc6e158ac4
Klara <klara.hirao@elpassion.pl>
2016-08-11 12:10:16 +0200

    Fix account orders tests

--- spec/requests/account/account_orders_spec.rb
+++ spec/requests/account/account_orders_spec.rb
@@ -10,24 +10,9 @@ describe AccountsOrders::Api, type: :request do
       end
 
       it 'should return orders of user' do
-        serialized_orders = OrderListSerializer.new(user.orders.page(1).per(5)).as_json
+        serialized_orders = OrderListSerializer.new(user.orders).as_json
         expect(response.body).to eq(serialized_orders.to_json)
         expect(response).to match_response_schema('account_orders')
-        expect(response).to have_http_status(:success)
-      end
-    end
-
-    context 'with pagination params' do
-      before(:each) do
-        2.times { create(:order, user: user) }
-        get '/api/v1/orders', { per_page: 1, page: 1 }, auth_header_for(user)
-        @user_orders = user.orders.page(1).per(1)
-      end
-
-      it 'should paginate orders of user' do
-        serialized_orders = OrderListSerializer.new(@user_orders).as_json
-        expect(response.body).to eq(serialized_orders.to_json)
-        expect(@user_orders.total_pages).to eq(2)
         expect(response).to have_http_status(:success)
       end
     end
