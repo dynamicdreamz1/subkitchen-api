6e876108f6cf60cf5cb0868e51bfcadcc9b22fc0
dynamicdreamz1 <info@dynamicdreamz.com>
2017-01-04 14:30:24 +0530

    Add CORS solution

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module SubKitchen
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
 
     # config.paths.add File.join('app'), glob: File.join('**', '*.rb')
     config.autoload_paths += %W(#{config.root}/app/api)
